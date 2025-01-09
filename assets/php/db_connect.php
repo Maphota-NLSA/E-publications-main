9bcbb90d3ae38172ae3c7c52fe92c8ddb8e8599e
nicholusmahlangu <nicholus.mahlangu@nlsa.ac.za>
2025-01-09 09:01:22 +0200

    9-1-2025 - Email notifications still works and I added the publisher's email address as part of the email body.

--- assets/php/db_connect.php
+++ assets/php/db_connect.php
@@ -97,7 +97,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                                 $mail->addAddress("nicholus.mahlangu@nlsa.ac.za","Nicholus");
 
                                                 $mail->Subject= "Submission of Electronic book";
-                                                $mail->Body="Hi Admin<br> A new book has been submitted";
+                                                $mail->Body="Hi Admin A new book has been submitted $email";
 
                                                 $mail->send();
                                                 echo "email sent";
