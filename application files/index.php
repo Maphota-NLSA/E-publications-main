9dd4a6dcbfe6805646652f16a008f4ebf4cfaf69
EugeneKeyz <eugenenhluvuko@gmail.com>
2025-01-16 08:32:18 +0200

    Header updated on home page
    Header fader has been updated Logo to white background.

--- application files/index.php
+++ application files/index.php
@@ -6,8 +6,8 @@
     <title>Home</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
-
-    <!-- Tab icon -->
+ghgsfgvhsefsdfsdg
+    <!-- Tab icon -->     
     <link href="../assets/img/favicon.webp" rel="icon">
     
     <!-- Google fonts -->
